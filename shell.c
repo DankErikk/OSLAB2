726b8b2782d644590feb95844ae3764d64c8e0b3
DankErikk <erikrivera40@gmail.com>
2020-02-25 19:31:03 -0800

    able to get username

--- shell.c
+++ shell.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 
 #define MAX 50 
 
 int main(){
+    printf("RESULT IS \n");
+    char* userName;
+    getlogin_r(userName, sizeof(userName));
+    printf("%s\n",userName);
 
     while(1){
         printf("ITERATING THROUGH WHILE LOOP\n");
-        printf("ps1 var is \u@\h:\W> \n");
+        //printf("ps1 var is \u@\h:\W> \n");
         char input[MAX];
         char checkExit[MAX] = "exit";
 
